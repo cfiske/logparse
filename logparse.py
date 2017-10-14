1cad3befc7d253b868f304a295f8194457e58490
Clay Fiske <clay_fiske@symantec.com>
2017-10-14 16:05:47 -0700

    Add support for outputting JSON logs in rsyslog-readable format
    Use a base dictionary for default values to prevent missing fields
    
    Fix setting of syslog facility/severity so they don't interfere
    with empty dict check
    
    Make timestamps use utcnow() vs now()
    
    Instead of separate validDate() use makeDate() and automatically
    get back a usable timestamp
    
    Misc code cleanup
    - Parse matching conditionals
    - Put various prints behind verbose conditionals
    -

--- logparse.py
+++ logparse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import re, time, random, sys, getopt
+import re, time, random, sys, getopt, json, copy
 
 from dateutil import parser
 from datetime import datetime
@@ -8,27 +8,43 @@ from datetime import datetime
 import Device
 
 
-def validDate(datestring):
-    try:
-        parser.parse(datestring)
-        return True
-    except ValueError:
-        print "%s is not a valid datetime" % datestring
-        return False
+def makeDate(datestring):
+    d = datetime.utcnow()
 
-opts, args = getopt.getopt(sys.argv[1:], "av")
+    try:
+        d = parser.parse(datestring)
+    except ValueError:
+        if verbose > 0:
+            print "makeDate: %s is not a valid datetime" % datestring
+
+    return str(d.strftime("%Y-%m-%dT%H:%M:%S.%f"))
+
+opts, args = getopt.getopt(sys.argv[1:], "ajv")
 
 matches = {}
 verbose = 0
+use_json = 0
 addtag = 0
 skipcount = 0
-minute = datetime.now().strftime("%M")
+minute = datetime.utcnow().strftime("%M")
+
+# define mandatory fields here
+baseDict = {
+    "host": 'UNKNOWN',
+    "facility": 'UNKNOWN',
+    "severity": 'debug',
+    "msg_type": 'UNKNOWN',
+    "message": 'UNKNOWN'
+}
+
 
 for o, a in opts:
     if o == '-v':
         verbose = 1
     if o == '-a':
         addtag = 1
+    if o == '-j':
+        use_json = 1
 
 with open("/dev/stdin") as f:
 
@@ -53,6 +69,7 @@ with open("/dev/stdin") as f:
     pats['text'] = [re.compile(r'(?P<text>\S.*)(?P<space>\s*)$')]
 
     currentDict = {}
+    jsonDict = copy.deepcopy(baseDict)
 
     messages = []
 
@@ -70,12 +87,11 @@ with open("/dev/stdin") as f:
                 continue
 
         if addtag == 1:
-            line = '<' + str(random.randint(0, 191)) + '>' + line
+            line = '<' + str(random.randint(9, 191)) + '>' + line
 
         rawline = line
 
         for pname in ['pri', 'date', 'host', 'text']:
-            #print "checking %s" % pname
             for p in pats[pname]:
                 matched = p.match(line)
 
@@ -84,8 +100,6 @@ with open("/dev/stdin") as f:
                     # of a new log message) and still have a populated
                     # object, dump it and reset the object
                     if pname == 'pri':
-                        currentDict['severity'] = int(matched.group('pri')) & 7
-                        currentDict['facility'] = int(matched.group('pri')) >> 3
                         if currentDict:
                             if verbose > 0:
                                 print currentDict
@@ -93,9 +107,12 @@ with open("/dev/stdin") as f:
                             messages.append(currentDict)
                             currentDict = {}
 
+                        else:
+                            currentDict['severity'] = int(matched.group('pri')) & 7
+                            currentDict['facility'] = int(matched.group('pri')) >> 3
+
                     if pname == 'date':
-                        if not validDate(matched.group('date').rstrip(':')):
-                            currentDict['date'] = time.strftime("%b %d %H:%M:%S", time.gmtime())
+                        currentDict['date'] = makeDate(matched.group('date').rstrip(':'))
 
                     currentDict[pname] = matched.group(pname)
 
@@ -109,34 +126,54 @@ with open("/dev/stdin") as f:
 
         if currentDict == {}:
             print "matched nothing: %s" % line
+
         elif currentDict['text'].find('last message repeated') == 0:
             continue
+
         else:
             skip = 0
-            #print "checking host %s" % currentDict['host']
+
             if currentDict['host'].find('v-') == 0:
                 if linux.matchLogPattern(currentDict):
                     if currentDict['state'] == 0:
                         skip = 1
                         skipcount += 1
+            elif juniper.matchLogPattern(currentDict):
+                if currentDict['state'] == 0:
+                    skip = 1
+                    skipcount += 1
             else:
-                if juniper.matchLogPattern(currentDict):
-                    if currentDict['state'] == 0:
-                        skip = 1
-                        skipcount += 1
-#            else:
-#                print "Did not match Juniper message: %s" % (currentDict['text'])
+                if verbose > 0:
+                    print "Did not match Juniper message: %s" % (currentDict['text'])
 
             if skip == 0:
-                messages.append(currentDict)
+                if use_json > 0:
+                    if 'date' not in currentDict:
+                        jsonDict['@timestamp'] = makeDate('now')
+                    else:
+                        jsonDict['@timestamp'] = currentDict['date']
+
+                    jsonDict['host'] = currentDict['host']
+                    jsonDict['message'] = currentDict['text']
+                    jsonDict['msg_type'] = currentDict['id']
+
+                    # mmjsonparse in rsyslog won't parse JSON correctly without
+                    # the '@cee:@cee:' prefix
+                    print "@cee:@cee:" + json.dumps(jsonDict)
+                else:
+                    if verbose > 0:
+                        print currentDict
+
                 currentDict = {}
 
-        if int(datetime.now().strftime("%M")) != minute:
+        if int(datetime.utcnow().strftime("%M")) != minute:
             minute = int(datetime.now().strftime("%M"))
-            print "%28s Messages parsed: %d  Skipped: %d" % (str(datetime.now()), len(messages), skipcount)
+            if verbose > 0:
+                print "%28s Messages parsed: %d  Skipped: %d" % (str(datetime.utcnow()), len(messages), skipcount)
 
 
-print "done"
+if verbose > 0:
+    print "done"
 
 
 for message in messages:
@@ -148,8 +185,9 @@ for message in messages:
 
         matches['host'][message['host']] = 1
 
-for h in matches['host'].keys():
-    print "%s: %d" % (h, matches['host'][h])
+if verbose > 0:
+    for h in matches['host'].keys():
+        print "%s: %d" % (h, matches['host'][h])
 
-print "Skipped: %d" % (skipcount)
+    print "Skipped: %d" % (skipcount)
 
